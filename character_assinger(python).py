777b69d46c2806f4bfabf05245d88a8a05a3bddb
Eli-Drew <drew313@live.missouristate.edu>
2020-09-05 02:18:56 -0500

    added character list txt file. still need to fix distribution function

--- character_assinger(python).py
+++ character_assinger(python).py
@@ -10,8 +10,36 @@ of characters, numbers vs strings, etc)
 
 ======================================
 """
-##from random import randint, shuffle
 import random
+    
+
+def readfile():
+    """Reads a text file to populate characters."""
+
+    character_lists = [[],[],[]]
+    
+    file = input("Enter the movie text file you want to use: ")
+    f = open(file + ".txt" , 'r')
+
+    list_name = 0
+    
+    for line in f:
+        line = line.strip()
+        if line == "Tier A":
+            continue
+        elif line == "Tier B":
+            list_name = 1
+            continue
+        elif line == "Tier C":
+            list_name = 2
+            continue
+        elif line == '':
+            continue
+        else:
+            character_lists[list_name].append(line)
+
+    return character_lists[0], character_lists[1], character_lists[2]
+        
 
 def create_character_list(tier_letter):
     """This function populates a list with characters
@@ -59,30 +87,47 @@ def assign_characters(character_list, users):
         assigned_list.append(character)
 
     return assigned_list, character_list
+
+def print_list(lst):
+    """Prints list in a desirable way."""
+    delimiter = ""
+    for x in lst:
+        print(delimiter, x)
+        
+
 def main():
 
-    tier_A_characters = create_character_list("Tier A")
-    tier_B_characters = create_character_list("Tier B")
-    tier_C_characters = create_character_list("Tier C")
+    tier_A_characters, tier_B_characters, tier_C_characters = readfile()
+
+##    tier_A_characters = create_character_list("Tier A")
+##    tier_B_characters = create_character_list("Tier B")
+##    tier_C_characters = create_character_list("Tier C")
+##    print(tier_A_characters)
+##    print()
+##
+##    print(tier_B_characters)
+##    print()
+##
+##    print(tier_C_characters)
+##    print()
 
     users = int(input("How many people are reading?: "))
 
 
     #this for loop with give character lists for users
     for x in range(users):
+
+        print("=" * 50)
         user_name = input("Enter the player's name: ")
+        #fix math for character distribution
         user_list_A, tier_A_characters = assign_characters(tier_A_characters, users)
-        print(user_list_A)
-        print()
+        print_list(user_list_A)
 
         user_list_B, tier_B_characters = assign_characters(tier_B_characters, users)
-        print(user_list_B)
-        print()
+        print_list(user_list_B)
 
         user_list_C, tier_C_characters = assign_characters(tier_C_characters, users)
-        print(user_list_C)
-        print()
+        print_list(user_list_C)
                                     
-        
 
 main()
